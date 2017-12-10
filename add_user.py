799977a8a4b945917f450d898cb9e26772754ca2
remmi11 <wtgeographer@gmail.com>
2017-12-10 13:57:02 -0600

    map.js updates

--- add_user.py
+++ add_user.py
@@ -14,27 +14,8 @@ engine = create_engine(connString, echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# user = User("kmcentire","gdipw5")
-# session.add(user)
-
-# user = User("jcox","gdipw4")
-# session.add(user)
-
-# user = User("jsiscoe","password")
-# session.add(user)
- 
-# user = User("mshaw","gdipw1")
-# session.add(user)
- 
-# user = User("scarr","gdipw2")
-# session.add(user)
- 
-# user = User("pventura","gdipw3")
-# session.add(user)
- 
-# user = User("hlemons","gdipw10")
-# session.add(user)
 user = User("nhuntington","gdipass2017")
 session.add(user)
+
 # commit the record the database
 session.commit()
--- static/js/map.js
+++ static/js/map.js
@@ -8,6 +8,17 @@ var map = new L.Map('map', {
     detectRetina: true
 });
 
+// $.getJSON("/static/js/cupcakes.json", function(data) {
+//     var geojson = L.geoJson(data, {
+//         onEachFeature: function (feature, layer) {
+//             layer.bindPopup(feature.properties.name);
+//         }
+//     });
+//     var map = L.map('map').fitBounds(geojson.getBounds());
+//     cupcakeTiles.addTo(map);
+//     geojson.addTo(map);
+// });
+
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
@@ -16,6 +27,13 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     id: 'mapbox.streets'
 }).addTo(map);
 
+//$.getJSON("/static/js/tracts.json", function(data) { addDataToMap(data, map); });
 
-
-
+$.getJSON("/static/js/cupcakes.json", function(data) {
+    var geojson = L.geoJson(data, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    }).addTo(map);
+    map.fitBounds(geojson.getBounds());
+});
